a4f9710b4adb0161fb0be2c5f050b90c0db5eac0
Yuxuan Zhang <github@z-yx.cc>
2025-04-10 21:03:16 -0400

    update util.ts: improve NameGen

--- src/util.ts
+++ src/util.ts
@@ -17,31 +17,71 @@ export function crash(...messages: string[]): never {
     throw error;
 }
 
+function* range(s: number, e: number) {
+    for (let i = s; i < e; i++) {
+        yield i;
+    }
+}
+
+function str(codes: Iterable<number>) {
+    return String.fromCharCode(...codes);
+}
+
+// 0-9: 48-57
+// A-Z: 65-90
+// a-z: 97-122
+const A_Z = str(range(65, 90 + 1));
+const a_z = str(range(97, 122 + 1));
+const digits = str(range(48, 57 + 1));
+
 export class NameGenerator {
-    private letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    private counter = 0;
-    private excludes: Set<string>;
+    /**
+     * Letters used for name composition
+     */
+    static readonly lead = A_Z + a_z;
+    static readonly rest = A_Z + a_z + digits;
+    static convert(n: number): string {
+        const { lead, rest } = NameGenerator;
+        let result = '';
+        while (n >= lead.length) {
+            result = rest[n % rest.length] + result;
+            n = Math.floor(n / rest.length);
+        }
+        return lead[n] + result;
+    }
+
+    public readonly excludes: Set<string>;
 
     constructor(excludes: Iterable<string> = []) {
         this.excludes = new Set<string>([...keywords, ...excludes]);
     }
 
+    /**
+     * Already calculated but not yet used by next()
+     */
+    private cached: string | undefined;
+    /**
+     * Peek the next generated name without consuming it.
+     */
+    get peek() {
+        if (this.cached === undefined) {
+            this.cached = this.next();
+        }
+        return this.cached;
+    }
+
+    private counter = 0;
+
     next(): string {
-        let next: string;
-        do {
-            const result: number[] = [];
-            let n = this.counter;
-            this.counter++;
-            do {
-                result.push(n % this.letters.length);
-                n = Math.floor(n / this.letters.length);
-            } while (n > 0);
-            next = result
-                .reverse()
-                .map((c) => this.letters[c])
-                .join('');
-        } while (this.excludes.has(next));
-        return next;
+        if (this.cached !== undefined) {
+            const next = this.cached;
+            this.cached = undefined;
+            return next;
+        }
+        while (true) {
+            const next = NameGenerator.convert(this.counter++);
+            if (!this.excludes.has(next)) return next;
+        }
     }
 }
 
